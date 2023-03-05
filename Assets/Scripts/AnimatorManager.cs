789c0a73d0240757e5bfb91e2111e92a9db10ab7
Francesco Logli <francesco.logli@yahoo.it>
2023-03-05 23:44:17 +0100

    Camera - Follow Target

--- Assets/Scripts/AnimatorManager.cs
+++ Assets/Scripts/AnimatorManager.cs
@@ -2,73 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimatorManager : MonoBehaviour
+namespace Utopia
 {
-
-    Animator animator;
-    int horizontal;
-    int vertical;
-
-    private void Awake()
+    public class AnimatorManager : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        horizontal = Animator.StringToHash("Horizontal");
-        vertical = Animator.StringToHash("Vertical");
 
-    }
-    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
-    {
-        float snappedHorizontal;
-        float snappedVertical;
+        Animator animator;
+        int horizontal;
+        int vertical;
 
-        #region Snapped Horizontal
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            horizontal = Animator.StringToHash("Horizontal");
+            vertical = Animator.StringToHash("Vertical");
 
-        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
-        {
-            snappedHorizontal = 0.5f;
         }
-        else if (horizontalMovement > 0.55f)
+        public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
         {
-            snappedHorizontal = 1f;
-        }
-        else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
-        {
-            snappedHorizontal = -0.5f;
-        }
-        else if (horizontalMovement < -0.55f)
-        {
-            snappedHorizontal = -1;
-        }
-        else
-        {
-            snappedHorizontal = 0;
-        }
-        #endregion
-        
-        #region Snapped Vertical
-        if (verticalMovement > 0 && verticalMovement < 0.55f)
-        {
-            snappedVertical = 0.5f;
-        }
-        else if (verticalMovement > 0.55f)
-        {
-            snappedVertical = 1f;
-        }
-        else if (verticalMovement < 0 && verticalMovement > -0.55f)
-        {
-            snappedVertical = -0.5f;
-        }
-        else if (verticalMovement < -0.55f)
-        {
-            snappedVertical = -1;
-        }
-        else
-        {
-            snappedVertical = 0;
-        }
-        #endregion
+            float snappedHorizontal;
+            float snappedVertical;
 
-        animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
-        animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+            #region Snapped Horizontal
+
+            if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+            {
+                snappedHorizontal = 0.5f;
+            }
+            else if (horizontalMovement > 0.55f)
+            {
+                snappedHorizontal = 1f;
+            }
+            else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+            {
+                snappedHorizontal = -0.5f;
+            }
+            else if (horizontalMovement < -0.55f)
+            {
+                snappedHorizontal = -1;
+            }
+            else
+            {
+                snappedHorizontal = 0;
+            }
+            #endregion
+
+            #region Snapped Vertical
+            if (verticalMovement > 0 && verticalMovement < 0.55f)
+            {
+                snappedVertical = 0.5f;
+            }
+            else if (verticalMovement > 0.55f)
+            {
+                snappedVertical = 1f;
+            }
+            else if (verticalMovement < 0 && verticalMovement > -0.55f)
+            {
+                snappedVertical = -0.5f;
+            }
+            else if (verticalMovement < -0.55f)
+            {
+                snappedVertical = -1;
+            }
+            else
+            {
+                snappedVertical = 0;
+            }
+            #endregion
+
+            animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
+            animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+        }
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 namespace Utopia
 {
-public class PlayerManager : MonoBehaviour
-{
-    InputManager inputManager;
-    PlayerLocomotion playerLocomotion;
-
-    private void Awake() 
+    public class PlayerManager : MonoBehaviour
     {
-        inputManager = GetComponent<InputManager>();
-        playerLocomotion = GetComponent<PlayerLocomotion>();
-    }
+        InputManager inputManager;
+        CameraManager cameraManager;
+        PlayerLocomotion playerLocomotion;
 
-    private void Update() 
-    {
-        inputManager.HandleAllInputs();
-    }
+        private void Awake()
+        {
+            inputManager = GetComponent<InputManager>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+            cameraManager = FindObjectOfType<CameraManager>();
+        }
 
-    private void FixedUpdate() 
-    {
-        playerLocomotion.HandleAllMovement();
+        private void Update()
+        {
+            inputManager.HandleAllInputs();
+        }
+
+        private void FixedUpdate()
+        {
+            playerLocomotion.HandleAllMovement();
+        }
+
+        private void LateUpdate() 
+        {
+            cameraManager.FollowTarget();
+        }
     }
-}
 }
